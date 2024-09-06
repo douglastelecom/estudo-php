27f899972feb24da74d20351f4276ad1ddad53fc
Douglas Fernandes <douglastelec@gmail.com>
2024-09-06 16:59:31 -0300

    fsdf

---
+++ strings.php
@@ -0,0 +1,9 @@
+<?php
+$idade = 28;
+//diferença entre aspas simples e duplas
+echo 'Olá mundo! Minha idade é ' . $idade . ' anos';
+echo "Olá mundo! Minha idade é $idade anos";
+
+//quebrar linha
+ echo "Olá mundo! \n";
+ echo "Olá mundo!";
--- tipos.php
+++ tipos.php
@@ -1,4 +1,9 @@
 <?php
 
 $idade = 21;
-$salario = 1001;
+$salario = 1001.30;
+$texto = "Olá mundo";
+$verdadeiro = true;
+$falso = false;
+//descobrir o tipo das variáveis
+echo gettype($salario);
